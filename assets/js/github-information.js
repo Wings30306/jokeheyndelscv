9c33daf8b999970ccc3de9a3c15d19d346edd0bb
Joke Heyndels <jo_hannah@outlook.com>
2018-11-10 21:22:19 +0100

    Setting up basic lay-out based on existing "resume" exercise

---
+++ assets/js/github-information.js
@@ -0,0 +1,90 @@
+function userInformationHTML (user) {
+  return `
+      <h2>Find me on Github 
+          <span class="small-name">
+              @ <a href="${user.html_url}" target="_blank">${user.login}</a>
+          </span>
+      </h2>
+      <div class="gh-content">
+          <div class="gh-avatar">
+              <a href="${user.html_url}" target="_blank">
+                  <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+              </a>
+          </div>
+          <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+      </div>`
+}
+
+function repoInformationHTML (repos) {
+  if (repos.length == 0) {
+    return `<div class="clearfix repo-list">No repos!</div>`
+  }
+  
+  var listItemsHTML = repos.map(function (repo) {
+    if (repo.homepage != null) {
+    return `<tr>
+                <th>
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </th>
+                <td>${repo.description}</td>
+                <td>${repo.language}</td>
+                <td>
+                   <a href="${repo.homepage}" target="_blank">Live site</a></td>
+              </tr>`
+  }
+  })
+
+  return `<div class="clearfix repo-list">
+              <table>
+                <tr>
+                  <th>Repo Name</th>
+                  <th>Description</th>
+                  <th>Language</th>
+                  <th>Deployed site</th>
+                </tr>
+                  ${listItemsHTML.join('\n')}
+              </table>
+          </div>`
+}
+
+function fetchGitHubInformation (event) {
+  $("#gh-user-data").html("")
+  $("#gh-repo-data").html("")
+  var username = "wings30306"
+
+  $('#gh-user-data').html(
+    `<div id="loader">
+          <img src="assets/css/loader.gif" alt="loading..." />
+      </div>`
+  )
+
+  $.when(
+    $.getJSON(`https://api.github.com/users/${username}`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)
+  ).then(
+    function (firstResponse, secondResponse) {
+      var userData = firstResponse[0]
+      var repoData = secondResponse[0]
+      $('#gh-user-data').html(userInformationHTML(userData))
+      $('#gh-repo-data').html(repoInformationHTML(repoData))
+    },
+    function (errorResponse) {
+      if (errorResponse.status === 404) {
+        $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`)
+      } else if(errorResponse.status === 403) {
+        var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset")*1000);
+        $("#gh-user-data").html(
+          `<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`
+        )
+      } else {
+        console.log(errorResponse)
+        $('#gh-user-data').html(
+          `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
+        )
+      }
+    }
+  )
+}
+
+
+$(document).ready(fetchGitHubInformation);
---
+++ assets/js/sendEmail.js
@@ -0,0 +1,28 @@
+function sendMail(contactForm) {
+  emailjs.send("gmail","contact_resume",{
+    "from_name": contactForm.name.value,
+    "from_email": contactForm.emailaddress.value,
+    "project_request": contactForm.projectsummary.value,
+  })
+  .then(
+    function(response) {
+    console.log("SUCCESS", response);
+  }, 
+  function(error) {
+    console.log("FAILED", error)
+  })
+  .then(
+    function(response) {
+       alert("Your message has been sent");
+        window.location = "contact.html";
+    },
+    function(error) {
+        console.log("FAILED", error);
+        alert('Your message could not be sent. Please try again');
+        window.location = 'contact.html';
+    }
+);
+return false;  // To block from loading a new page
+;
+}
+
