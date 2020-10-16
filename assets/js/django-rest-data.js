97b5efd82457488169ee69d77dff9d2d6dd2beca
Joke Heyndels <jo_hannah@outlook.com>
2020-10-16 10:48:07 +0200

    New language feature: change project description based on language

--- assets/js/django-rest-data.js
+++ assets/js/django-rest-data.js
@@ -1,10 +1,11 @@
-const projectDiv = document.getElementById("project-content");
+const projectDivNL = document.getElementById("project-content-nl");
+const projectDivEN = document.getElementById("project-content-en");
 
 
 const url = "https://jh-portfolio-api.herokuapp.com/projects/";
 
 async function loadProjects() {
-  let projects = await fetch(url)
+  let projectsNl = await fetch(url)
     .then(response => response.json())
     .then(data => data.results)
     .then(data =>
@@ -14,7 +15,27 @@ async function loadProjects() {
         <div class="col-12 col-sm-6 col-md-4 col-lg-4 card">
               
     <a href="${repo.live_link}" target="_blank" class="btn btn-info uppercase">${repo.title}</a>
-    
+    <p>${repo.description_nl}</p>
+    <p>
+      <a class="btn btn-info" href="${repo.code_link}" target="_blank">
+      <i class="fab fa-github"></i>&nbsp CODE</a>
+    </p>
+  </div>
+  `
+        )
+        .join("\n")
+    )
+
+    let projectsEn = await fetch(url)
+    .then(response => response.json())
+    .then(data => data.results)
+    .then(data =>
+      data
+        .map(
+          (repo) => `
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4 card">
+              
+    <a href="${repo.live_link}" target="_blank" class="btn btn-info uppercase">${repo.title}</a>
     <p>${repo.description}</p>
     <p>
       <a class="btn btn-info" href="${repo.code_link}" target="_blank">
@@ -26,8 +47,12 @@ async function loadProjects() {
         .join("\n")
     )
 
-  projectDiv.innerHTML = `<div class="row row-table">${projects}</div>`
-}
+    projectDivNL.innerHTML = `<div class="row row-table">${projectsNl}</div>`
+    projectDivEN.innerHTML = `<div class="row row-table">${projectsEn}</div>`
+  }
+
+loadProjects()
+
 
 
 
